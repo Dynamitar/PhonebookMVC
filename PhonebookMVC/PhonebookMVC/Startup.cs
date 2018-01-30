0f352a391cbe1fd278bef7066891e96508e6a542
Dimitar Gochev <dimitarggochev@gmail.com>
2018-01-30 22:21:45 +0200

    first commit

---
+++ PhonebookMVC/PhonebookMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhonebookMVC.Startup))]
+namespace PhonebookMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
